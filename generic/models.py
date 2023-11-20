2b83132d40c9f3f539b40b2558be4bdcc7a0a792
4kbr <igyutubfesbuk@gmail.com>
2023-11-20 16:12:42 +0700

    create snipet

--- generic/models.py
+++ generic/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 from wagtail.models import Page
 from wagtail.admin.panels import FieldPanel
+from wagtail.snippets.models import register_snippet
+
+
 
 # Create your models here.
 class GenericPage(Page):
@@ -18,8 +21,42 @@ class GenericPage(Page):
     related_name='+',
   )
 
+  author = models.ForeignKey(
+    'Author',
+    null=True,
+    blank=False,
+    on_delete=models.SET_NULL,
+    related_name='+',
+  )
+
   content_panels = Page.content_panels + [
     FieldPanel("banner_title"),
     FieldPanel("introduction"),
     FieldPanel("banner_image"),
+    FieldPanel("author"),
   ]
+
+@register_snippet
+class Author(models.Model):
+  name = models.CharField(max_length=100)
+  title = models.CharField(blank=True, max_length=100)
+  company_name = models.CharField(blank=True, max_length=100)
+  company_url = models.CharField(blank=True, max_length=100)
+  image = models.ForeignKey(
+    'wagtailimages.Image',
+    on_delete=models.SET_NULL,
+    null=True,
+    blank=False,
+    related_name='+'
+  )
+
+  panels = [
+    FieldPanel("name"),
+    FieldPanel("title"),
+    FieldPanel("company_name"),
+    FieldPanel("company_url"),
+    FieldPanel("image"),
+  ]
+
+  def __str__(self) -> str:
+    return self.name;
